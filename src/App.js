f03cd767f037aa291b8d6ef14d3517407149efa1
Siddhant Yadav <your.passionate.idiots@gmail.com>
2021-06-16 07:22:00 +0530

    Static Dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter , Switch , Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
-
+import Dashboard from "./components/dashboard/Dashboard"
 
 function App() {
   return (
@@ -10,9 +10,18 @@ function App() {
       <div classname="App">
         {/* <h1>Planning App by Siddhant Yadav</h1> */}
         <Navbar />
+
+        <Switch >
+          <Route path = '/' component = {Dashboard} />
+        </Switch>
+
+
+
       </div>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+<Dashboard />
