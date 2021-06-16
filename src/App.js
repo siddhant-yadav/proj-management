b3ddd946eb074fe820723d083bd8d14ac04dbcbe
Siddhant Yadav <your.passionate.idiots@gmail.com>
2021-06-16 09:44:31 +0530

    Added Project Details

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter , Switch , Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard"
+import ProjectDetails from "./components/projects/ProjectDetails";
+
 
 function App() {
   return (
@@ -12,10 +14,10 @@ function App() {
         <Navbar />
 
         <Switch >
-          <Route path = '/' component = {Dashboard} />
+          <Route exact path = '/' component = {Dashboard} />
+          <Route exact path = '/project/:id' component = {ProjectDetails} />
         </Switch>
 
-        
 
       </div>
     </BrowserRouter>
