a3b53502c69fd87df96914762d0476483901c760
Siddhant Yadav <your.passionate.idiots@gmail.com>
2021-06-16 06:19:13 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div>
+      <h1>Planning App by Siddhant Yadav</h1>
+    </div>
+  )
+}
+
+export default App
