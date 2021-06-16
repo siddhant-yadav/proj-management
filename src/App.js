be49ca168c016f296ef03ccdcb04551f4d4450a2
Siddhant Yadav <your.passionate.idiots@gmail.com>
2021-06-16 23:10:12 +0530

    Login and SignUp

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import { BrowserRouter , Switch , Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard"
 import ProjectDetails from "./components/projects/ProjectDetails";
-
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
-      <div classname="App">
+      <div classname="App black">
         {/* <h1>Planning App by Siddhant Yadav</h1> */}
         <Navbar />
 
         <Switch >
           <Route exact path = '/' component = {Dashboard} />
           <Route exact path = '/project/:id' component = {ProjectDetails} />
+          <Route exact path = '/signin' component = {SignIn} />
+          <Route exact path = '/signup' component = {SignUp} />
         </Switch>
 
 
---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+
+class SignUp extends Component {
+
+    state = {
+        email : "",
+        password : "",
+        confirmPassword : "",
+        firstName : "",
+        lastName : ""
+    }
+    
+    handleChange = (e) =>
+    {
+        this.setState(
+            {
+                [e.target.id] : e.target.value
+            }
+            )
+    }
+        
+        
+    handleSubmit = (e) =>
+    { return(
+        e.preventDefault(),
+        (this.state.confirmPassword === this.state.password) ? ( console.log(this.state) ) : alert("pass don't Match")
+        
+    )}
+
+    
+    render() {
+
+    
+        return (
+            <div className = "container">
+                <form onSubmit = {this.handleSubmit} className = "white" >
+                    <h5 className = "grey-text text-darken-3">SignIn</h5>
+
+                    <div className = "input-field" >
+                        <label >First Name</label>
+                        <input type = "text" id = "firstName" onChange = {this.handleChange} />
+                    </div>
+
+                    <div className = "input-field" >
+                        <label >Last Name</label>
+                        <input type = "text" id = "lastName" onChange = {this.handleChange} />
+                    </div>
+
+
+                    <div className = "input-field" >
+                        <label htmlFor = "email" >e-mail</label>
+                        <input type = "email" id = "email" onChange = {this.handleChange} />
+                    </div>
+                
+                    <div className = "input-field" >
+                        <label htmlFor = "password" >Password</label>
+                        <input type = "password" id = "password" onChange = {this.handleChange} />
+                    </div>
+                
+                    <div className = "input-field" >
+                        <label htmlFor = "password" >Confirm Password</label>
+                        <input type = "password" id = "confirmPassword" onChange = {this.handleChange} />
+                    </div>
+                
+                    <div className = "input-field" >
+                        <button className = "btn blue lighten-1 z-depth-0" >Sign Up</button>
+                    </div>
+
+                </form>
+                
+            </div>
+        )
+    }
+}
+
+export default SignUp
